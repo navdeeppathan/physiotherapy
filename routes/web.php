67f37bbf1a7d22105359a2bb6efd21432229a4df
infoharry99 <infoharry99@gmail.com>
2026-06-27 17:35:33 +0530

    DS

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,6 @@ use Illuminate\Support\Facades\Route;
 
  use App\Http\Controllers\AppointmentTransferRequestController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [AuthController::class, 'showLogin'])->name('admin.login');
 

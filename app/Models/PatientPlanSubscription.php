9dd997258ab2486f5036f37ee1c1cd099cffc9a0
infoharry99 <infoharry99@gmail.com>
2026-09-07 22:22:08 +0530

    Include 3-digit random number in unique_plan_id generator

--- app/Models/PatientPlanSubscription.php
+++ app/Models/PatientPlanSubscription.php
@@ -34,14 +34,15 @@ class PatientPlanSubscription extends Model
 
     /**
      * Generate unique plan ID
-     * e.g., PLN-20260907-P11-9A8B
+     * e.g., PLN-20260907-P11-9A8B-482
      */
     public static function generateUniquePlanId($patientId = null): string
     {
-        $dateStr = date('Ymd');
-        $patStr  = $patientId ? "P{$patientId}" : 'PAT';
-        $randStr = strtoupper(substr(bin2hex(random_bytes(3)), 0, 5));
-        return "PLN-{$dateStr}-{$patStr}-{$randStr}";
+        $dateStr  = date('Ymd');
+        $patStr   = $patientId ? "P{$patientId}" : 'PAT';
+        $randHex  = strtoupper(substr(bin2hex(random_bytes(2)), 0, 4));
+        $rand3Num = rand(100, 999);
+        return "PLN-{$dateStr}-{$patStr}-{$randHex}-{$rand3Num}";
     }
 
     /*
